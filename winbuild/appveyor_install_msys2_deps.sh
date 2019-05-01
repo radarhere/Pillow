55b92e8e305b9629b0878e950d248f73a0d24e2d
Andrew Murray <radarhere@users.noreply.github.com>
2019-05-01 11:48:35 +1000

    openjpeg2

--- winbuild/appveyor_install_msys2_deps.sh
+++ winbuild/appveyor_install_msys2_deps.sh
@@ -6,7 +6,7 @@ pacman -S --noconfirm mingw32/mingw-w64-i686-python3-pip \
 	   mingw32/mingw-w64-i686-python2-pip \
 	   mingw32/mingw-w64-i686-python2-setuptools \
 	   mingw-w64-i686-libjpeg-turbo \
-	   mingw-w64-i686-libwebp
+	   mingw-w64-i686-openjpeg2
 
 C:/msys64/mingw32/bin/python3 -m pip install --upgrade pip
 
