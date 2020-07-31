c456586b29dbda89deba86de877a0d78c7925287
Andrew Murray <radarhere@users.noreply.github.com>
2020-07-31 21:00:34 +1000

    justpip

--- winbuild/appveyor_install_msys2_deps.sh
+++ winbuild/appveyor_install_msys2_deps.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
 mkdir /var/cache/pacman/pkg
-pacman -S --noconfirm mingw32/mingw-w64-i686-python3-pip \
-     mingw-w64-i686-libjpeg-turbo \
-
-C:/msys64/mingw32/bin/python3 -m pip install --upgrade pip
-
-/mingw32/bin/pip3 install setuptools
+pacman -S --noconfirm mingw32/mingw-w64-i686-python3-pip
