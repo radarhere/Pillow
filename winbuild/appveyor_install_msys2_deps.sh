4d8a15f10962329100824f4cf3f0782114483327
Andrew Murray <radarhere@users.noreply.github.com>
2020-07-31 20:46:39 +1000

    just

--- winbuild/appveyor_install_msys2_deps.sh
+++ winbuild/appveyor_install_msys2_deps.sh
@@ -2,11 +2,8 @@
 
 mkdir /var/cache/pacman/pkg
 pacman -S --noconfirm mingw32/mingw-w64-i686-python3-pip \
-     mingw32/mingw-w64-i686-python3-setuptools \
-     mingw32/mingw-w64-i686-python3-pytest \
      mingw-w64-i686-libjpeg-turbo \
 
 C:/msys64/mingw32/bin/python3 -m pip install --upgrade pip
 
-/mingw32/bin/pip install olefile
-/mingw32/bin/pip3 install olefile
+/mingw32/bin/pip3 install setuptools
