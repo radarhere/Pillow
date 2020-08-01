501fa4c12c73c6bf84a58eb14151883a261bb1fe
Andrew Murray <radarhere@users.noreply.github.com>
2020-08-01 21:05:43 +1000

    eh

--- winbuild/appveyor_install_msys2_deps.sh
+++ winbuild/appveyor_install_msys2_deps.sh
@@ -10,14 +10,7 @@ pacman -U --noconfirm --ask 20 msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
 echo "FIRST4"
 pacman -Sy --noconfirm --ask 20
 echo "FIRST5"
-pacman -Sy --noconfirm --ask 20 mingw32/mingw-w64-i686-python3-pip \
+pacman -Sy --noconfirm --ask 20
      mingw32/mingw-w64-i686-python3-setuptools \
-     mingw32/mingw-w64-i686-python3-pytest \
-     mingw32/mingw-w64-i686-python3-pytest-cov \
      mingw-w64-i686-libjpeg-turbo \
      mingw-w64-i686-libimagequant
-
-C:/msys64/mingw32/bin/python3 -m pip install --upgrade pip
-
-/mingw32/bin/pip install olefile
-/mingw32/bin/pip3 install olefile
