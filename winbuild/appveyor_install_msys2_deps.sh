fa277269d991ff62bc6b29fde920e42d773c37a9
Andrew Murray <radarhere@users.noreply.github.com>
2020-08-01 12:39:35 +1000

    alt

--- winbuild/appveyor_install_msys2_deps.sh
+++ winbuild/appveyor_install_msys2_deps.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 mkdir /var/cache/pacman/pkg
-pacman -Syu
-pacman -S --noconfirm mingw32/mingw-w64-i686-python3-pip \
+pacman -Syu --noconfirm mingw32/mingw-w64-i686-python3-pip \
      mingw32/mingw-w64-i686-python3-setuptools \
      mingw32/mingw-w64-i686-python3-pytest \
      mingw32/mingw-w64-i686-python3-pytest-cov \
