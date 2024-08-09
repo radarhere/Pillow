f8701e947b9f76cad78fcb163105007637c120d2
Andrew Murray <radarhere@users.noreply.github.com>
2024-08-09 19:58:34 +1000

    alt

--- src/PIL/ImageCms.py
+++ src/PIL/ImageCms.py
@@ -33,7 +33,7 @@ try:
     from . import _imagingcms as core
 
     _CmsProfileCompatible = Union[
-        str, SupportsRead[bytes], "!core.CmsProfile", "ImageCmsProfile"
+        str, SupportsRead[bytes], core.CmsProfile, "ImageCmsProfile"
     ]
 except ImportError as ex:
     # Allow error import for doc purposes, but error out when accessing
@@ -41,6 +41,13 @@ except ImportError as ex:
     from ._util import DeferredError
 
     core = DeferredError.new(ex)
+
+    class CmsProfile:
+        pass
+
+    _CmsProfileCompatible = Union[
+        str, SupportsRead[bytes], CmsProfile, "ImageCmsProfile"
+    ]
 
 _DESCRIPTION = """
 pyCMS
