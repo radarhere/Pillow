de1fce9b31e0787ab9eb2ecbb1e86d534c3fd20f
Andrew Murray <radarhere@users.noreply.github.com>
2024-08-09 20:37:54 +1000

    yep

--- src/PIL/ImageCms.py
+++ src/PIL/ImageCms.py
@@ -32,10 +32,8 @@ from ._typing import SupportsRead
 try:
     from . import _imagingcms as core
 
-    CmsProfile = core.CmsProfile
-
     _CmsProfileCompatible = Union[
-        str, SupportsRead[bytes], CmsProfile, "ImageCmsProfile"
+        str, SupportsRead[bytes], core.CmsProfile, "ImageCmsProfile"
     ]
 except ImportError as ex:
     # Allow error import for doc purposes, but error out when accessing
@@ -49,7 +47,7 @@ except ImportError as ex:
         class CmsProfile:
             pass
 
-        _CmsProfileCompatible = Union[
+        _CmsProfileCompatible = Union[  # type: ignore[misc]
             str, SupportsRead[bytes], CmsProfile, "ImageCmsProfile"
         ]
 
