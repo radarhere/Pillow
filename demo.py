dd9d8157a65affdc53d55f5109d2b0e6e6d40cb9
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-10 23:02:34 +1100

    Test 7042

---
+++ demo.py
@@ -0,0 +1,17 @@
+import tempfile
+import threading
+from PIL import Image
+
+def test():
+    with Image.open("3-pages.tiff") as im, tempfile.NamedTemporaryFile('wb') as f:
+        im.save(f, format="TIFF", compression="tiff_lzw")
+
+threads = []
+
+for i in range(32):
+    t = threading.Thread(target=test, daemon=True)
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
