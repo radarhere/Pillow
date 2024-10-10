18dae715aeecd2dfd2a67c59045dacffbd9f40be
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-10 12:51:09 +1100

    Phase 2 with oss-fuzz/test_fuzzers.py

--- .ci/test.sh
+++ .ci/test.sh
@@ -4,4 +4,4 @@ set -e
 
 python3 -c "from PIL import Image"
 
-python3 -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests $REVERSE
+python3 -bb -m pytest --threads 1 --iterations 1 -v -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests/oss-fuzz/test_fuzzers.py $REVERSE
