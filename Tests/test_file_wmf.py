97a3ce19c4fde865b80f058dce885400204fcc24
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-13 23:40:01 +1100

    Resize

--- Tests/test_file_wmf.py
+++ Tests/test_file_wmf.py
@@ -13,5 +13,11 @@ from .helper import assert_image_similar_tofile, hopper
 def test_load_raw() -> None:
     # Test basic EMF open and rendering
     with Image.open("image94.emf") as im:
+        im.load()
         print(im)
-        im.save('out.png')
+        width, height = im.size
+        new_width = 400
+        new_height = int((new_width / width) * height)
+        resized_image = im.resize((new_width, new_height), Image.LANCZOS)
+        print(resized_image)
+        resized_image.save('out.png')
