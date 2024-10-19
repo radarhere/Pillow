5c50e4d7690d8d3f5628261555de3babb29f780f
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-20 06:54:14 +1100

    resize

--- Tests/test_file_wmf.py
+++ Tests/test_file_wmf.py
@@ -12,6 +12,11 @@ from .helper import assert_image_similar_tofile, hopper
 
 def test_6980() -> None:
     with Image.open("image100.emf") as im:
-        im.load()
-        print(im, im.info)
-        im.save("out.png")
+        width, height = im.size
+        new_width = 400
+        new_height = (new_width / width) * height
+        if len(im.info["dpi"]) == 2:
+            new_height /= im.info["dpi"][1] / im.info["dpi"][0]
+        new_height = int(new_height)
+        resized_image = im.resize((new_width, new_height), Image.LANCZOS)
+        resized_image.save("out.png")
