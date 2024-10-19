5563bf9f98d60c260e918fbed1df91273bb8886a
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-20 06:47:59 +1100

    100

--- Tests/test_file_wmf.py
+++ Tests/test_file_wmf.py
@@ -11,7 +11,7 @@ from .helper import assert_image_similar_tofile, hopper
 
 
 def test_6980() -> None:
-    with Image.open("image94.emf") as im:
+    with Image.open("image100.emf") as im:
         im.load()
         print(im, im.info)
         im.save("out.png")
