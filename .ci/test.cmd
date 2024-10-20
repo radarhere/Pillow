6724663aa8030991de8d287b5563ff48b0babf13
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-20 23:30:08 +1100

    find

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,5 +1,5 @@
 copy %GITHUB_WORKSPACE%\winbuild\build\bin\fribidi.dll .
-copy %GITHUB_WORKSPACE%\winbuild\build\bin\freetype.dll .
-copy %GITHUB_WORKSPACE%\winbuild\build\bin\cairo.dll .
+dir /s c:/ freetype.dll
+dir /s c:/ cairo.dll
 python.exe -m pip install mplcairo matplotlib
 python.exe demo.py
