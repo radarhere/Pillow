1ca0e0a3e91d7778bd146dce36f5f9d2f1705d4b
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-20 22:55:53 +1100

    faster

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,3 +1,2 @@
-python.exe -c "from PIL import Image"
-IF ERRORLEVEL 1 EXIT /B
-python.exe -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests
+python.exe -m pip install mplcairo matplotlib
+python.exe demo.py
---
+++ demo.py
@@ -0,0 +1,6 @@
+import mplcairo
+import matplotlib
+
+matplotlib.use("module://mplcairo.tk")
+
+mplcairo.set_options(raqm=True)
