1a083744e1d8b7d41436d1bd0f31bc7a29b276cf
Andrew Murray <radarhere@users.noreply.github.com>
2024-10-21 13:44:05 +1100

    dir

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,4 +1,14 @@
 copy %GITHUB_WORKSPACE%\winbuild\build\bin\fribidi.dll .
+copy %GITHUB_WORKSPACE%\winbuild\build\bin\fribidi.dll libfribidi.dll
+copy %GITHUB_WORKSPACE%\winbuild\build\bin\fribidi.dll libfribidi-0.dll
+copy %GITHUB_WORKSPACE%\winbuild\build\lib\brotlicommon.lib .
+copy %GITHUB_WORKSPACE%\winbuild\build\lib\brotlidec.lib .
 copy %GITHUB_WORKSPACE%\winbuild\build\lib\freetype.lib .
+copy %GITHUB_WORKSPACE%\winbuild\build\lib\harfbuzz.lib .
+copy %GITHUB_WORKSPACE%\libraqm-0.dll libraqm.dll
+
+dir %GITHUB_WORKSPACE%\winbuild\build\bin
+dir %GITHUB_WORKSPACE%\winbuild\build\lib
+
 python.exe -m pip install mplcairo matplotlib
 python.exe demo.py
