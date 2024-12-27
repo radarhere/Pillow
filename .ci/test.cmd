821d3e19b0c0fd200625fd92dbcd6e55ce3189ae
Andrew Murray <radarhere@users.noreply.github.com>
2024-12-27 20:04:24 +1100

    fuzz

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,3 +1,3 @@
 python.exe -c "from PIL import Image"
 IF ERRORLEVEL 1 EXIT /B
-python.exe -bb -m pytest -s -v -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests/test_file_jpeg2k.py
+python.exe -c "from PIL import Image;Image.open('clusterfuzz-testcase-minimized-fuzz_pillow-5015640213159936').load()"
