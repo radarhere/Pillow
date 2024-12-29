ed1415937cd178442ee47be5241527ca5c133856
Andrew Murray <radarhere@users.noreply.github.com>
2024-12-30 00:28:37 +1100

    fix

--- src/decode.c
+++ src/decode.c
@@ -109,7 +109,6 @@ _dealloc(ImagingDecoderObject *decoder) {
         decoder->cleanup(&decoder->state);
     }
     free(decoder->state.buffer);
-    decoder->state.buffer = NULL;
     free(decoder->state.context);
     Py_XDECREF(decoder->lock);
     Py_XDECREF(decoder->state.fd);
