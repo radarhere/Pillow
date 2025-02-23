e5f42399fe449d80c7b8ee678c1ece7ec0f79d48
Andrew Murray <radarhere@users.noreply.github.com>
2025-02-23 14:27:46 +1100

    debug

--- .github/workflows/wheels-dependencies.sh
+++ .github/workflows/wheels-dependencies.sh
@@ -41,8 +41,9 @@ ZLIB_NG_VERSION=2.2.4
 
 function build_zlib_ng {
     if [ -e zlib-stamp ]; then return; fi
-    fetch_unpack https://github.com/zlib-ng/zlib-ng/archive/$ZLIB_NG_VERSION.tar.gz zlib-ng-$ZLIB_NG_VERSION.tar.gz
-    (cd zlib-ng-$ZLIB_NG_VERSION \
+    git clone https://github.com/radarhere/zlib-ng
+    (cd zlib-ng \
+        && git checkout develop \
         && ./configure --prefix=$BUILD_PREFIX --zlib-compat --debug \
         && make -j4 \
         && make install)
