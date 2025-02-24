35efdc0d36059a56a7a5f3bcf3349e87d7ec7ab9
Andrew Murray <radarhere@users.noreply.github.com>
2025-02-25 07:11:13 +1100

    branch

--- .github/workflows/wheels-dependencies.sh
+++ .github/workflows/wheels-dependencies.sh
@@ -64,8 +64,9 @@ function build_pkg_config {
 
 function build_zlib_ng {
     if [ -e zlib-stamp ]; then return; fi
-    fetch_unpack https://github.com/zlib-ng/zlib-ng/archive/$ZLIB_NG_VERSION.tar.gz zlib-ng-$ZLIB_NG_VERSION.tar.gz
-    (cd zlib-ng-$ZLIB_NG_VERSION \
+    git clone https://github.com/phprus/zlib-ng
+    (cd zlib-ng \
+        && git checkout macos-legacy-1 \
         && ./configure --prefix=$BUILD_PREFIX --zlib-compat \
         && make -j4 \
         && make install)
