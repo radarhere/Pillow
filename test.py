7c492175f22730bac1988cdd339e88850155de82
Andrew Murray <radarhere@users.noreply.github.com>
2025-02-27 18:48:14 +1100

    pass

--- test.py
+++ test.py
@@ -1,5 +1,30 @@
-from PIL import TiffImagePlugin
+from PIL import Image, ImageSequence, TiffImagePlugin
 
+# Preparation
+import numpy as np
+images = [np.asarray(im) for im in [
+    Image.new("RGB", (100, 100), "#f00"),
+    Image.new("RGB", (100, 100), "#0f0")
+]]
+listOfTuples = [
+    (55, [1, 3]),
+    (56, [2, 4])
+]
+
+# Script
+image_list = []
+for image in images:
+    image_list.append(Image.fromarray(image))
+frames = []
+for i, frame in enumerate(image_list):
+    info = TiffImagePlugin.ImageFileDirectory()
+
+    for tupleIdTag, tupleTags in listOfTuples:
+        info[tupleIdTag] = tupleTags[i]
+        info.tagtype[tupleIdTag] = 3
+
+    frame.encoderinfo = {'tiffinfo': info}
+    frames.append(frame)
 with open("out.tiff", "w+b") as fp:
     with TiffImagePlugin.AppendingTiffWriter(fp) as tf:
         print("closed1" if tf.f.closed else "not closed1")
