231759529ee2f3b59bd949bf2301a6418cf01acf
Andrew Murray <radarhere@users.noreply.github.com>
2025-02-27 18:59:00 +1100

    fail2

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -8,11 +8,5 @@ fi
 brew install \
     jpeg-turbo \
     libtiff
-export PKG_CONFIG_PATH="/usr/local/opt/openblas/lib/pkgconfig"
 
 python3 -m pip install coverage
-python3 -m pip install -U pytest
-python3 -m pip install -U pytest-cov
-python3 -m pip install -U pytest-timeout
-python3 -m pip install pyroma
-python3 -m pip install numpy
--- test.py
+++ test.py
@@ -1,37 +1,13 @@
 from PIL import Image, ImageSequence, TiffImagePlugin
 
-# Preparation
-import numpy as np
-images = [np.asarray(im) for im in [
-    Image.new("RGB", (100, 100), "#f00"),
-    Image.new("RGB", (100, 100), "#0f0")
-]]
-listOfTuples = [
-    (55, [1, 3]),
-    (56, [2, 4])
-]
-
-# Script
-image_list = []
-for image in images:
-    image_list.append(Image.fromarray(image))
-frames = []
-for i, frame in enumerate(image_list):
-    info = TiffImagePlugin.ImageFileDirectory()
-
-    for tupleIdTag, tupleTags in listOfTuples:
-        info[tupleIdTag] = tupleTags[i]
-        info.tagtype[tupleIdTag] = 3
-
-    frame.encoderinfo = {'tiffinfo': info}
-    frames.append(frame)
 with open("out.tiff", "w+b") as fp:
     with TiffImagePlugin.AppendingTiffWriter(fp) as tf:
-        frame = frames[0]
+        frame = Image.new("RGB", (100, 100), "#f00")
         frame.encoderconfig = ()
-        print("_save", i)
+        frame.encoderinfo = {}
+        print("_save")
         TiffImagePlugin._save(frame, tf, "out.tiff")
-        print("_save end", i)
+        print("_save end")
         print("closed1" if tf.f.closed else "not closed1")
         tf.newFrame()
         print("closed2" if tf.f.closed else "not closed2")
