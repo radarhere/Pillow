dad5fd17eed97f902b1bd067fff477f23fd11b7b
Andrew Murray <radarhere@users.noreply.github.com>
2025-03-17 19:25:33 +1100

    direct

--- Tests/test_file_tiff.py
+++ Tests/test_file_tiff.py
@@ -35,6 +35,6 @@ class TestFileTiff:
     def test_timeout(self, monkeypatch: pytest.MonkeyPatch) -> None:
         import time
         start = time.time()
-        with Image.open("Tests/images/timeout-6646305047838720") as im:
+        with TiffImagePlugin.TiffImageFile("Tests/images/timeout-6646305047838720") as im:
             monkeypatch.setattr(ImageFile, "LOAD_TRUNCATED_IMAGES", True)
         print(time.time() - start)
