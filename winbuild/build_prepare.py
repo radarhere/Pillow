3d87cf8648da1bc7adc3ee5d6b2cae49eb997dca
Andrew Murray <radarhere@users.noreply.github.com>
2025-04-06 07:44:19 +1000

    Try both

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -181,7 +181,7 @@ DEPS: dict[str, dict[str, Any]] = {
         "filename": f"xz-{V['XZ']}.tar.gz",
         "license": "COPYING",
         "build": [
-            *cmds_cmake("liblzma", "-DBUILD_SHARED_LIBS:BOOL=OFF", "-DCMAKE_C_FLAGS=\"-DXZ_CLMUL_CRC=OFF\""),
+            *cmds_cmake("liblzma", "-DBUILD_SHARED_LIBS:BOOL=OFF", "-DCMAKE_C_FLAGS=\"-DLZMA_LZ_DECODER_CONFIG=1 -DXZ_CLMUL_CRC=OFF\""),
             cmd_mkdir(r"{inc_dir}\lzma"),
             cmd_copy(r"src\liblzma\api\lzma\*.h", r"{inc_dir}\lzma"),
         ],
