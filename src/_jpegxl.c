c4a59d42c2ba6ac2761b3b280b9431555c35ee75
Andrew Murray <radarhere@users.noreply.github.com>
2025-08-16 20:52:49 +1000

    single

--- src/_jpegxl.c
+++ src/_jpegxl.c
@@ -293,6 +293,34 @@ decoder_loop_skip_process:
         _jxl_decoder_count_frames((PyObject *)decp);
     }
 
+    JxlFrameHeader fhdr = {};
+
+    printf("torchget_next %d\n", decp->status);
+    // process events until next frame output is ready
+    while (decp->status != JXL_DEC_NEED_IMAGE_OUT_BUFFER) {
+        decp->status = JxlDecoderProcessInput(decp->decoder);
+        printf("status %d\n", decp->status);
+
+        // every frame was decoded successfully
+        if (decp->status == JXL_DEC_SUCCESS) {
+            printf("torchnone\n");
+            Py_RETURN_NONE;
+        }
+
+        // this should only occur after rewind
+        if (decp->status == JXL_DEC_NEED_MORE_INPUT) {
+            _jxl_decoder_set_input((PyObject *)decp);
+            continue;
+        }
+
+        if (decp->status == JXL_DEC_FRAME) {
+            // decode frame header
+            decp->status = JxlDecoderGetFrameHeader(decp->decoder, &fhdr);
+            continue;
+        }
+    }
+    printf("single done\n");
+
     return (PyObject *)decp;
 }
 
