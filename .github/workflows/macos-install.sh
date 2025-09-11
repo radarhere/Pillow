df7a68fe80a0c9a5ec22dcc893b760bf25970548
Andrew Murray <radarhere@users.noreply.github.com>
2025-09-11 20:26:21 +1000

    faster214

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -2,22 +2,18 @@
 
 set -e
 
+brew update
 if [[ "$ImageOS" == "macos13" ]]; then
     brew uninstall gradle maven
 fi
 brew install \
-    aom \
-    dav1d \
     freetype \
-    ghostscript \
     jpeg-turbo \
     libimagequant \
     libraqm \
     libtiff \
     little-cms2 \
     openjpeg \
-    rav1e \
-    svt-av1 \
     webp
 export PKG_CONFIG_PATH="/usr/local/opt/openblas/lib/pkgconfig"
 
@@ -29,13 +25,6 @@ python3 -m pip install -U pytest
 python3 -m pip install -U pytest-cov
 python3 -m pip install -U pytest-timeout
 python3 -m pip install pyroma
-python3 -m pip install numpy
-# optional test dependency, only install if there's a binary package.
-# fails on beta 3.14 and PyPy
-python3 -m pip install --only-binary=:all: pyarrow || true
-
-# libavif
-pushd depends && ./install_libavif.sh && popd
 
 # extra test images
 pushd depends && ./install_extra_test_images.sh && popd
