603593ac6de75f7d357f4e091c7c229b1bca0326
Andrew Murray <radarhere@users.noreply.github.com>
2025-09-11 18:20:30 +1000

    No build or remove

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-brew uninstall freetype ant cairo fontconfig gradle harfbuzz kotlin maven openjdk selenium-server
 brew install libpng
-cd winbuild/depends/freetype-2.14.0 && ./configure --with-png=yes && sudo make install
 brew install jpeg-turbo
