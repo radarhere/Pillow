cf3ef0ee3cee39a606170aa76cc69a152b529ff9
Andrew Murray <radarhere@users.noreply.github.com>
2025-09-14 22:30:23 +1000

    test

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -7,42 +7,7 @@ brew install libpng automake libtool jpeg-turbo
 
 git clone https://github.com/freetype/freetype.git
 cd freetype
-
-echo "Log: 2.13.3 will pass"
-git checkout VER-2-13-3
-sh autogen.sh
-./configure --with-harfbuzz=no --with-png=yes && sudo make install
-
-cd ..
-make clean
-make install
-python3 -m PIL.report
-python3 demo.py
-
-cd freetype
-echo "Log: The last passing commit"
-git checkout VER-2-14-0
-git reset --hard HEAD~172
-
-sh autogen.sh
-./configure --with-harfbuzz=no --with-png=yes && sudo make install
-
-cd ..
-make clean
-make install
-python3 -m PIL.report
-python3 demo.py
-
-cd freetype
-if [[ $VERSION == "master" ]]; then
-  echo "Log: Master fails"
-  git checkout master
-else
-  echo "Log: The first failing commit"
-  git checkout VER-2-14-0
-  git reset --hard HEAD~171
-  git show
-fi
+git checkout master
 sh autogen.sh
 ./configure --with-harfbuzz=no --with-png=yes && sudo make install
 
