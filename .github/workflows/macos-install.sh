da0511462fb84bae4ece9860d45da2181c527550
Andrew Murray <radarhere@users.noreply.github.com>
2025-09-16 00:06:25 +1000

    test

--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -4,6 +4,9 @@ set -e
 
 brew uninstall freetype ant cairo fontconfig gradle harfbuzz kotlin maven openjdk selenium-server
 HOMEBREW_NO_INSTALL_FROM_API=1 brew install --build-from-source radarhere/homebrew-freetypetap/freetype
+brew test freetype
+brew audit --strict freetype
+brew style freetype
 brew install libpng automake libtool jpeg-turbo
 
 make clean
