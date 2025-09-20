c4949cc6fa9e358e61b35290471bee94e7892272
Andrew Murray <radarhere@users.noreply.github.com>
2025-09-20 13:06:11 +1000

    novar

--- .github/workflows/wheels-test.sh
+++ .github/workflows/wheels-test.sh
@@ -6,15 +6,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # If Homebrew is on the path during the build, it may leak into the wheels.
     # However, we *do* need Homebrew to provide a copy of fribidi for
     # testing purposes so that we can verify the fribidi shim works as expected.
-    HOMEBREW_PREFIX=/opt/homebrew
-    arch -arm64 $HOMEBREW_PREFIX/bin/brew install fribidi
+    arch -arm64 /opt/homebrew/bin/brew install fribidi
 
     # Add the lib folder for fribidi so that the vendored library can be found.
     # Don't use $HOMEWBREW_PREFIX/lib directly - use the lib folder where the
     # installed copy of fribidi is cellared. This ensures we don't pick up the
     # Homebrew version of any other library that we're dependent on (most notably,
     # freetype).
-    export DYLD_LIBRARY_PATH=$(dirname $(realpath $HOMEBREW_PREFIX/lib/libfribidi.dylib))
+    export DYLD_LIBRARY_PATH=$(dirname $(realpath /opt/homebrew/lib/libfribidi.dylib))
 elif [ "${AUDITWHEEL_POLICY::9}" == "musllinux" ]; then
     apk add curl fribidi
 else
