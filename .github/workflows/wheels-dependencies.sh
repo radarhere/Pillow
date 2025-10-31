c3446aa99c7c91835179f836efc2ec2f64233d77
Andrew Murray <radarhere@users.noreply.github.com>
2025-10-31 17:58:25 +1100

    bindir

--- .github/workflows/wheels-dependencies.sh
+++ .github/workflows/wheels-dependencies.sh
@@ -166,7 +166,7 @@ function build_brotli {
     local out_dir=$(fetch_unpack https://github.com/google/brotli/archive/v$BROTLI_VERSION.tar.gz brotli-$BROTLI_VERSION.tar.gz)
     echo "torchstart"
     (cd $out_dir \
-        && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX -DCMAKE_INSTALL_LIBDIR=$BUILD_PREFIX/lib -DCMAKE_INSTALL_NAME_DIR=$BUILD_PREFIX/lib $HOST_CMAKE_FLAGS . \
+        && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX -DCMAKE_INSTALL_LIBDIR=$BUILD_PREFIX/lib -DCMAKE_INSTALL_NAME_DIR=$BUILD_PREFIX/lib -DCMAKE_INSTALL_BINDIR=$BUILD_PREFIX/bin $HOST_CMAKE_FLAGS . \
         && echo "torchmid" \
         && make -j4 install)
     echo "torchend"
