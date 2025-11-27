a602842ca5b369685a88edefe65286e104bdcef8
Andrew Murray <radarhere@users.noreply.github.com>
2025-11-27 20:45:14 +1100

    print

--- .github/workflows/wheels-dependencies.sh
+++ .github/workflows/wheels-dependencies.sh
@@ -177,15 +177,18 @@ function build_brotli {
 function build_jpegxl {
     if [ -e jpegxl-stamp ]; then return; fi
 
+    echo "torch1"
     local out_dir=$(fetch_unpack https://github.com/google/highway/archive/1.3.0.tar.gz)
     (cd $out_dir \
         && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX -DCMAKE_INSTALL_LIBDIR=$BUILD_PREFIX/lib -DCMAKE_INSTALL_NAME_DIR=$BUILD_PREFIX/lib $HOST_CMAKE_FLAGS . \
         && make install)
 
+    echo "torch2"
     local out_dir=$(fetch_unpack https://github.com/libjxl/libjxl/archive/v$JPEGXL_VERSION.tar.gz)
     (cd $out_dir \
         && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX -DCMAKE_INSTALL_LIBDIR=$BUILD_PREFIX/lib -DCMAKE_INSTALL_NAME_DIR=$BUILD_PREFIX/lib -DJPEGXL_ENABLE_SJPEG=OFF -DJPEGXL_ENABLE_SKCMS=OFF -DBUILD_TESTING=OFF $HOST_CMAKE_FLAGS . \
         && make install)
+    echo "torch3"
     touch jpegxl-stamp
 }
 
