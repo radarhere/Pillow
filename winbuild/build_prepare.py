30b3c788d05c1aa67463a061ba69ac69c75d4884
Andrew Murray <radarhere@users.noreply.github.com>
2025-12-27 22:09:46 +1100

    c

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -147,7 +147,7 @@ DEPS: dict[str, dict[str, Any]] = {
         },
         "build": [
             "vcpkg install highway",
-            "dir /vcpkg/packages/highway_x64-windows/",
+            r"dir C:/vcpkg/packages/highway_x64-windows/",
             *cmds_cmake(
                 ("jpeg-static", "djpeg-static"),
                 "-DENABLE_SHARED:BOOL=FALSE",
