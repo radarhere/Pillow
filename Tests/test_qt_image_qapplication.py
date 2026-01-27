54766b144860a38faaf960caece28b59a7522fb5
Andrew Murray <radarhere@users.noreply.github.com>
2026-01-27 22:07:09 +1100

    qa

--- Tests/test_qt_image_qapplication.py
+++ Tests/test_qt_image_qapplication.py
@@ -56,8 +56,9 @@ def roundtrip(expected: Image.Image) -> None:
 def test_sanity(tmp_path: Path) -> None:
     # Segfault test
     app: QApplication | None = QApplication([])
-    ex = Example()  # noqa: F841
+    ex = Example()
     assert app  # Silence warning
+    assert ex  # Silence warning
 
     for mode in ("1", "RGB", "RGBA", "L", "P"):
         # to QPixmap
