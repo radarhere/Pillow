22b98a752eca301e5b9515cc732d31d7862292bc
Andrew Murray <radarhere@users.noreply.github.com>
2026-02-19 23:53:14 +1100

    Without avifImageDestroy()

--- src/_avif.c
+++ src/_avif.c
@@ -227,7 +227,6 @@ AvifEncoderNew(PyObject *self_, PyObject *args) {
     memset(rgb.pixels, 255, rgb.rowBytes * frame->height);
 
     avifImageRGBToYUV(frame, &rgb);
-    avifImageDestroy(frame);
 
     PyErr_SetString(PyExc_RuntimeError, "end with error");
     return NULL;
