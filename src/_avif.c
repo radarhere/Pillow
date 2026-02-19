67a6b08deef77e6635cb5e1ba43ff94a1e15bfa3
Andrew Murray <radarhere@users.noreply.github.com>
2026-02-19 19:14:55 +1100

    nothreads

--- src/_avif.c
+++ src/_avif.c
@@ -530,9 +530,7 @@ _encoder_add(AvifEncoderObject *self, PyObject *args) {
     // rgb.pixels is safe for writes
     memcpy(rgb.pixels, rgb_bytes, size);
 
-    Py_BEGIN_ALLOW_THREADS;
     result = avifImageRGBToYUV(frame, &rgb);
-    Py_END_ALLOW_THREADS;
 
     if (result != AVIF_RESULT_OK) {
         PyErr_Format(
