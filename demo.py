c8cf111324afaf8577bf82e3cd9c7f4ee0ddedee
Andrew Murray <radarhere@users.noreply.github.com>
2026-05-14 00:32:44 +1000

    rename

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
-import os, tempfile
+import os, shutil, tempfile
 from PIL.ImageShow import WindowsViewer
 
 viewer = WindowsViewer()
-x = viewer.get_command("Tests/images/hopper.png")
+y = 'Tests/images/h"oppe"r.png'
+shutil.move("Tests/images/hopper.png", y)
+x = viewer.get_command(y)
 print(x)
