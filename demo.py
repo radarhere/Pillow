67d4f7b425667efcfc6f023617533a6ea03315b6
Andrew Murray <radarhere@users.noreply.github.com>
2026-05-14 00:36:58 +1000

    double

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import os, shutil, tempfile
 from PIL.ImageShow import WindowsViewer
 
 viewer = WindowsViewer()
-y = 'Tests/images/h\"oppe\"r.png'
+y = 'Tests/images/h\\"oppe\\"r.png'
 shutil.move("Tests/images/hopper.png", y)
 x = viewer.get_command(y)
 print(x)
