2060155048d2c56a255b496218d959c481fe0df6
Andrew Murray <radarhere@users.noreply.github.com>
2026-06-29 20:01:29 +1000

    Test ImagePath overflow

--- .ci/test.cmd
+++ .ci/test.cmd
@@ -1,3 +1,3 @@
 python.exe -c "from PIL import Image"
 IF ERRORLEVEL 1 EXIT /B
-python.exe -bb -m pytest -vv -x -W always --cov PIL --cov Tests --cov-report term --cov-report xml Tests
+python.exe -c "from PIL import ImagePath;ImagePath.Path(0x4000000000000000)"
