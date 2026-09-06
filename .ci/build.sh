531771ed2f3ad077485b6f336d3c050b37a51ace
Andrew Murray <radarhere@users.noreply.github.com>
2026-09-06 13:38:29 +1000

    faster

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,4 +4,8 @@ set -e
 
 python3 -m coverage erase
 make clean
+
+export CC="clang -fsanitize=undefined -fno-sanitize-recover=undefined"
+export CXX="clang++ -fsanitize=undefined -fno-sanitize-recover=undefined"
+export LDFLAGS="-fsanitize=undefined"
 make install-coverage
--- .github/workflows/macos-install.sh
+++ .github/workflows/macos-install.sh
@@ -6,18 +6,9 @@ brew bundle --file=.github/workflows/Brewfile
 export PKG_CONFIG_PATH="/usr/local/opt/openblas/lib/pkgconfig"
 
 python3 -m pip install coverage
-python3 -m pip install defusedxml
-python3 -m pip install ipython
-python3 -m pip install olefile
 python3 -m pip install -U pytest
 python3 -m pip install -U pytest-cov
 python3 -m pip install -U pytest-timeout
-# optional test dependencies, only install if there's a binary package.
-python3 -m pip install --only-binary=:all: numpy || true
-python3 -m pip install --only-binary=:all: pyarrow || true
-
-# libavif
-pushd depends && ./install_libavif.sh && popd
 
 # extra test images
 pushd depends && ./install_extra_test_images.sh && popd
