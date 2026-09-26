6e101a1b5ce5aae08b14053e249cc2af267e66bd
Andrew Murray <radarhere@users.noreply.github.com>
2026-09-26 13:53:38 +1000

    Revert "Use PyConfig instead of deprecated Py_SetPythonHome"
    This reverts commit 1673e8e3823b7aa25917672be6b2f60dce08c836.

--- Tests/test_image_access.py
+++ Tests/test_image_access.py
@@ -47,9 +47,7 @@ int main(int argc, char* argv[])
 {{
     char *home = "{home}";
     wchar_t *whome = Py_DecodeLocale(home, NULL);
-    PyConfig config;
-    PyConfig_InitPythonConfig(&config);
-    config.home = whome;
+    Py_SetPythonHome(whome);
 
     Py_InitializeEx(0);
     Py_DECREF(PyImport_ImportModule("PIL.Image"));
@@ -60,7 +58,6 @@ int main(int argc, char* argv[])
     Py_Finalize();
 
     PyMem_RawFree(whome);
-    PyConfig_Clear(&config);
 
     return 0;
 }}
