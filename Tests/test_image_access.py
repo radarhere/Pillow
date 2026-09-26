0f930e2d4a09938b8bbaa2d98e56b33554b31195
Andrew Murray <radarhere@users.noreply.github.com>
2026-09-26 14:37:07 +1000

    deprecate

--- Tests/test_image_access.py
+++ Tests/test_image_access.py
@@ -48,9 +48,7 @@ int main(int argc, char* argv[])
 {{
     char *home = "{home}";
     wchar_t *whome = Py_DecodeLocale(home, NULL);
-    PyConfig config;
-    PyConfig_InitPythonConfig(&config);
-    config.home = whome;
+    Py_SetPythonHome(whome);
 
     Py_InitializeEx(0);
     Py_DECREF(PyImport_ImportModule("PIL.Image"));
@@ -61,7 +59,6 @@ int main(int argc, char* argv[])
     Py_Finalize();
 
     PyMem_RawFree(whome);
-    PyConfig_Clear(&config);
 
     return 0;
 }}
