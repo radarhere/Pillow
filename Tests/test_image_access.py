9eb43eaecb991830fa985515b90ba997bebba385
Andrew Murray <radarhere@users.noreply.github.com>
2026-09-26 14:25:04 +1000

    plain

--- Tests/test_image_access.py
+++ Tests/test_image_access.py
@@ -33,7 +33,7 @@ class TestEmbeddable:
             "INCLUDEPY"
         ).replace("include", "libs")
         print("torch_libdir", libdir)
-        compiler.add_library(os.path.join(libdir, "libpython3"))
+        compiler.add_library("libpython3")
         compiler.add_library_dir(libdir)
         for path in os.listdir(libdir):
             if "ython" in path:
